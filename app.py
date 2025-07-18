bc1768f66c71ac55f7474660d41926f8ac4f0ef0
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-07-18 10:53:56 +0200

    PLPG-API-8: Refactored get topics by id endpoint for more efficiency

--- app.py
+++ app.py
@@ -24,5 +24,13 @@ def get_skills():
     skills = data_manager.read_data(SKILLS_FILE)
     return jsonify(skills)
 
+@app.route('/topics/<id>', methods=['GET'])
+def get_topics_by_id(id):
+    topics = data_manager.read_data(TOPICS_FILE)
+    topic = next((topic for topic in topics if topic.get('id').lower() == id.lower()), None)
+    return jsonify(topic)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
